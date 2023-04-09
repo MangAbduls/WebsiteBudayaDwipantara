587456b54f6a29e2d8061b99a7eafef4cf4690aa
Bagas <bagaskalihputra.bkp@gmail.com>
2023-04-09 13:29:55 +0700

    home and login page update

--- website/website/settings.py
+++ website/website/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
