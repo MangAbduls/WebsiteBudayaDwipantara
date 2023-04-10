4b6cdba9522a986d3910ebd388b40ea239534e3c
Bagas <bagaskalihputra.bkp@gmail.com>
2023-04-11 02:40:10 +0700

    fix minor bugs

--- website/login/views.py
+++ website/login/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from django.contrib import auth
 from django.contrib import messages
+from django.shortcuts import redirect
 
 
 # Create your views here.
-
-
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('nama')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        if username == '' or email == '' or password == '':
+            messages.error(request, 'Semua kolom harus diisi.')
+            return render(request, 'login.html')
         user = auth.authenticate(
             request, username=username, email=email, password=password)
         if user is not None:
             auth.login(request, user)
-            return render(request, 'home.html')
+            return redirect('home')
         else:
             messages.error(request, 'Akun tidak ditemukan.')
             return render(request, 'login.html')
