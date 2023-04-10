e3165ffee7dce39afc60c965557046f88516a8ab
Bagas <bagaskalihputra.bkp@gmail.com>
2023-04-11 04:33:31 +0700

    optimize base.html

--- website/login/views.py
+++ website/login/views.py
@@ -23,3 +23,23 @@ def login(request):
             return render(request, 'login.html')
     else:
         return render(request, 'login.html')
+
+
+def login2(request):
+    if request.method == 'POST':
+        username = request.POST.get('nama')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if username == '' or email == '' or password == '':
+            messages.error(request, 'Semua kolom harus diisi.')
+            return render(request, 'login2.html')
+        user = auth.authenticate(
+            request, username=username, email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Akun tidak ditemukan.')
+            return render(request, 'login2.html')
+    else:
+        return render(request, 'login2.html')
