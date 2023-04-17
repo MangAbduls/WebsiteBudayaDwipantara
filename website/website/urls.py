0b30f415a25d22f02ced9717b1784cf56aa0eefc
Bagas <bagaskalihputra.bkp@gmail.com>
2023-04-18 00:59:27 +0700

    add login logout feature and fix few bugs

--- website/website/urls.py
+++ website/website/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('login.urls')),
+    path('search/', include('search.urls')),
+    path('logout/', views.logout_view, name='logout'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
